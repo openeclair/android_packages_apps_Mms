634d30541ea5bf645850cc5d5498de2f7d5fc576
Wes Garner <wesgarner@gmail.com>
2010-03-08 13:24:00 -0600

    Useragent patch by Chris Banes

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -60,6 +60,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity {
 	public static final String CONVERSATION_FONT_SIZE      = "pref_key_mms_conversation_font_size";
 	public static final String CONVERSATION_HIDE_NAMES = "pref_key_conversation_hide_names";
 	public static final String CONVERSATION_LEFT_RIGHT = "pref_key_conversation_left_right";
+    public static final String USER_AGENT                   = "pref_key_mms_user_agent";
+    public static final String USER_AGENT_CUSTOM            = "pref_key_mms_user_agent_custom";
+
 
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
